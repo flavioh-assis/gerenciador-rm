294f5072584868434c14444363b5d5715b2f984e
Flavio Assis <38504393+flaviohassis@users.noreply.github.com>
2021-07-05 16:10:28 -0300

    stylize the main page

--- app/renderer/pages/Main.js
+++ app/renderer/pages/Main.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 export default () => {
-  
   return (
     <div className='container'>
-      Working in progress
+      <div className='page'>
+        <div className='titulo'>
+          <p id='title'>GERENCIADOR DE RM</p>
+        </div>
+      </div>
     </div>
   )
 }
