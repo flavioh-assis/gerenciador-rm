f96475f4b0afef81742affccf9781c7eac450db5
Flavio Assis <38504393+flaviohassis@users.noreply.github.com>
2021-07-16 13:58:21 -0300

    Adds header

--- app/renderer/pages/Main.js
+++ app/renderer/pages/Main.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import FieldsTable from '../components/Fields&Table'
 
+const Header = '../../app/assets/images/header_mvmi.png'
+
 export default () => {
   return (
     <div className='container'>
       <div className='page'>
         <div className='titulo'>
+          <img
+            alt='EMEB. "Maria Virgínia Matarazzo Ippólito'
+            id='header'
+            src={Header}
+          />
           <p id='title'>GERENCIADOR DE RM</p>
         </div>
-        
+
         <FieldsTable />
       </div>
     </div>
