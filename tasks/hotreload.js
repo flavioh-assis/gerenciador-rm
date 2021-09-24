fad8ec1c8af382168807a56934dfa552ed888a0a
Flavio Assis <flavioh.dev@gmail.com>
2021-09-24 10:29:44 -0300

    reactive hotreload

--- tasks/hotreload.js
+++ tasks/hotreload.js
@@ -1,48 +1,44 @@
-// const { series, src, dest } = require('gulp');
-// const inject = require('gulp-inject-string');
+const { series, src, dest } = require('gulp');
+const inject = require('gulp-inject-string');
 
-// const browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
 
-// function startBrowserSync(done) {
-//   browserSync.init(
-//     {
-//       ui: false,
-//       localOnly: true,
-//       port: 35829,
-//       ghostMode: false,
-//       open: false,
-//       notify: false,
-//       logSnippet: false,
-//       socket: {
-//         domain: 'http://localhost:3000'
-//       },
-//       // browser: 'firefox'
-//       browser: 'google chrome'
-//     },
-//     function (error) {
-//       done(error);
-//     },
-//   );
-// }
+function startBrowserSync(done) {
+  browserSync.init(
+    {
+      ui: false,
+      localOnly: true,
+      port: 35829,
+      ghostMode: false,
+      open: false,
+      notify: false,
+      logSnippet: false,
+      browser: 'firefox',
+    },
+    function (error) {
+      done(error);
+    },
+  );
+}
 
-// function injectBrowserSync() {
-//   return src('app/renderer/index.html')
-//     .pipe(inject.before('</body>', browserSync.getOption('snippet')))
-//     .pipe(
-//       inject.after('script-src', " 'unsafe-eval' " + browserSync.getOption('urls').get('local')),
-//     )
-//     .pipe(dest('build/renderer'));
-// }
+function injectBrowserSync() {
+  return src('app/renderer/index.html')
+    .pipe(inject.before('</body>', browserSync.getOption('snippet')))
+    .pipe(
+      inject.after('script-src', " 'unsafe-eval' " + browserSync.getOption('urls').get('local')),
+    )
+    .pipe(dest('build/renderer'));
+}
 
-// function reloadBrowser(done) {
-//   browserSync.reload();
-//   done();
-// }
+function reloadBrowser(done) {
+  browserSync.reload();
+  done();
+}
 
-// startBrowserSync.displayName = 'start-hotreload';
-// injectBrowserSync.displayName = 'inject-hotreload';
-// reloadBrowser.displayName = 'reload-hotreload';
+startBrowserSync.displayName = 'start-hotreload';
+injectBrowserSync.displayName = 'inject-hotreload';
+reloadBrowser.displayName = 'reload-hotreload';
 
-// exports.start = series(startBrowserSync, injectBrowserSync);
-// exports.inject = injectBrowserSync;
-// exports.reload = reloadBrowser;
+exports.start = series(startBrowserSync, injectBrowserSync);
+exports.inject = injectBrowserSync;
+exports.reload = reloadBrowser;
